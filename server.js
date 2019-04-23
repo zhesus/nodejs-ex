1f3aa1d662fc03b45857dd1f1352c93122f1e54d
zhesus <49912728+zhesus@users.noreply.github.com>
2019-04-23 17:30:31 +0000

    Update server.js

--- server.js
+++ server.js
@@ -76,22 +76,7 @@ var initDb = function(callback) {
 app.get('/', function (req, res) {
   // try to initialize the db on every request if it's not already
   // initialized.
-  if (!db) {
-    initDb(function(err){});
-  }
-  if (db) {
-    var col = db.collection('counts');
-    // Create a document with request IP and current time of request
-    col.insert({ip: req.ip, date: Date.now()});
-    col.count(function(err, count){
-      if (err) {
-        console.log('Error running count. Message:\n'+err);
-      }
-      res.render('index.html', { pageCountMessage : count, dbInfo: dbDetails });
-    });
-  } else {
-    res.render('index.html', { pageCountMessage : null});
-  }
+    res.send("homepage!!!!");
 });
 
 app.get('/pagecount', function (req, res) {
